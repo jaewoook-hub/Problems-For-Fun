ef3410e90cba8fae46ef99d14e516edac15d171b
Richard Ok <jaeok2017@u.northwestern.edu>
2021-01-30 22:44:53 -0600

    Fun Problems

--- Majority-Element.js
+++ Majority-Element.js
@@ -1,3 +1,14 @@
 var majorityElement = function(nums) {
+  const hash = {}
+  let max = 0, val
 
+  for (let i = 0; i < nums.length; i++){
+      hash[nums[i]] ? hash[nums[i]]++ : hash[nums[i]] = 1
+      if (hash[nums[i]] > max){
+          max = hash[nums[i]]
+          val = nums[i]
+      }
+  }
+
+  return val
 };
